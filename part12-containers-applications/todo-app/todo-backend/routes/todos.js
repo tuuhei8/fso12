1d905fba21356dda516f608a108f5bfcf9712086
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2026-02-10 15:02:44 +0200

    7/22 tehtävää tehty

--- part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,15 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.status(200).send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  req.todo.text = req.body.text
+  req.todo.done = req.body.done
+  const updatedTodo = await req.todo.save()
+  res.status(201).send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
